20955958c7962e48309b428b2d573ec054c3f242
jaredhettinger <44094375+jaredhettinger@users.noreply.github.com>
2019-01-13 19:06:42 -0600

    added character images, more game logic, message area

--- app/Game/character.component.js
+++ app/Game/character.component.js
@@ -8,9 +8,28 @@
         },
         controllerAs: 'vm',
         controller: function() {
-            var vm = this;
+            let vm = this;
 
             vm.characterClickCount = 0;
+            vm.characterSprite = '';
+
+            vm.$onInit = function() {
+                switch(vm.name) {
+                    case 'Bubblegum': vm.characterSprite = 'content/images/char_bubblegum.png'; break;
+                    case 'Capt. Birdman': vm.characterSprite = 'content/images/char_captainbirdman.png'; break;
+                    case 'Danky Kang': vm.characterSprite = 'content/images/char_dankykang.png'; break;
+                    case 'Dinosaur': vm.characterSprite = 'content/images/char_dinosaur.png'; break;
+                    case 'Fox': vm.characterSprite = 'content/images/char_fox.png'; break;
+                    case 'Jiggy Withit': vm.characterSprite = 'content/images/char_jiggywithit.png'; break;
+                    case 'Louie G': vm.characterSprite = 'content/images/char_louie.png'; break;
+                    case 'Mauricio': vm.characterSprite = 'content/images/char_mauricio.png'; break;
+                    case 'Ness': vm.characterSprite = 'content/images/char_ness.png'; break;
+                    case 'Sammy': vm.characterSprite = 'content/images/char_sammy.png'; break;
+                    case 'Sparkrat': vm.characterSprite = 'content/images/char_sparkrat.png'; break;
+                    case 'Zorldo': vm.characterSprite = 'content/images/char_zorldo.png'; break;
+                    default: break;
+                }
+            };
 
             vm.click = function() {
                 vm.characterClickCount++;
--- app/Game/game-app.component.js
+++ app/Game/game-app.component.js
@@ -7,10 +7,13 @@
             let vm = this;
 
             vm.victory = false;
+            vm.message = "Click around and see who else you can unlock!";
             vm.totalClickCount = 0;
 
+
             vm.checkForVictory = function() {
-                if(vm.totalClickCount >= 10) {
+                if(vm.totalClickCount >= 50) {
+                    vm.updateMessage();
                     vm.victory = true;
                 }
             };
@@ -18,6 +21,10 @@
             vm.clickCharacter = function() {
                 vm.totalClickCount++;
                 vm.checkForVictory();
+            };
+
+            vm.updateMessage = function () {
+                vm.message = "Wow you unlocked all the characters!";
             };
 
         },
